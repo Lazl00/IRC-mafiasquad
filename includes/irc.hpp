9bb43d11087d56831498ab16a7adb18b549f5a96
Lazl00 <lazloo.tp@gmail.com>
2026-04-20 14:57:59 +0200

    architecture

---
+++ includes/irc.hpp
@@ -0,0 +1,17 @@
+#ifndef IRC_HPP
+# define IRC_HPP
+
+#include <iostream>
+#include <string>       // container
+#include <vector>       // container
+#include <map>          // container
+#include <cstring>      // pour memset
+#include <cstdlib>      // pour l'exit
+#include <unistd.h>     // pour close
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <poll.h>       // gerer les inputs clients
+
+#endif
